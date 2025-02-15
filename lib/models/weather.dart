9b34c25d2392d020f3a914bbfafde23bd7b6810c
Navyrine <wahidadinugrohoo@gmail.com>
2025-02-15 22:08:54 +0800

    Membuat model class dan home page

---
+++ lib/models/weather.dart
@@ -0,0 +1,21 @@
+class Weather {
+  final String city;
+  final DateTime date;
+  final String weather;
+  final String description;
+  final double temperature;
+  final double wind;
+  final int humidity;
+  final int visibility;
+
+  const Weather({
+    required this.city,
+    required this.date,
+    required this.weather,
+    required this.description,
+    required this.temperature,
+    required this.wind,
+    required this.humidity,
+    required this.visibility,
+  });
+}
