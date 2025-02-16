8662c84e19be73ee4d4694ef4ee7af81e39bc5e7
Navyrine <wahidadinugrohoo@gmail.com>
2025-02-16 09:03:16 +0800

    Mengatur agar posisi layar tetap potrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weather_app/ui/pages/home_page.dart';
 
 void main() {
-  runApp(const HomePage());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      home: ProviderScope(child: const HomePage()),
+    );
+  }
 }
